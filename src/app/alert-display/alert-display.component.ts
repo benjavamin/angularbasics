6539fb2e05207d4e49e57ec5728df4466ea40cf2
Benjamin Bajic <benjamin.bajic@pragmatio.com>
2019-09-02 13:59:56 +0200

    generating components

---
+++ src/app/alert-display/alert-display.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '.alert',
+  templateUrl: './alert-display.component.html',
+  styleUrls: ['./alert-display.component.css']
+})
+export class AlertDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { AlertDisplayComponent } from './alert-display/alert-display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    AlertDisplayComponent
   ],
   imports: [
     BrowserModule
