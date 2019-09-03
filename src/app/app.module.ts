a8c3bcec450c81a7d24fbbf49a02736a2021db46
Benjamin Bajic <benjamin.bajic@pragmatio.com>
2019-09-03 10:30:13 +0200

    component generating practice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { AlertDisplayComponent } from './alert-display/alert-display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent,
-    AlertDisplayComponent
+    ServersComponent
   ],
   imports: [
     BrowserModule
