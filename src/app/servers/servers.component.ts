61528f6b46d3282cabb73fe0ce12053b819c7512
Benjamin Bajic <benjamin.bajic@pragmatio.com>
2019-09-04 15:52:08 +0200

    two way binding and practice exercise

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,7 +11,9 @@ export class ServersComponent implements OnInit {
 
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
-  serverName = '';
+  serverName = 'testServer';
+  username = '';
+
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -22,7 +24,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created';
+    this.serverCreationStatus = 'Server was created, Name is ' + this.serverName;
   }
 
   onUpdateServerName(event: Event) {
