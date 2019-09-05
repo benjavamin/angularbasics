89838a18e99861aab4d6fe42ede01e7bb64b3499
Benjamin Bajic <benjamin.bajic@pragmatio.com>
2019-09-05 10:39:31 +0200

    ngIf, Else. ngTemplate, ngStyle

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,9 +10,9 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
 
   allowNewServer = false;
-  serverCreationStatus = 'No server was created';
+  serverCreationStatus = '';
   serverName = 'testServer';
-  username = '';
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -25,6 +25,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server was created, Name is ' + this.serverName;
+    this.serverCreated = true;
   }
 
   onUpdateServerName(event: Event) {
